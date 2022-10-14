aecb2473bc73dc9393bc7d34cee06761d2e2c743
Gerardo Mikael do Carmo Pereira <B46994@fgv.edu.br>
2022-10-14 08:23:07 -0300

    adicionar cadastrar ingrediente

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,14 @@
 def cadastrar_ingrediente(tipo: str, ingrediente: str, preco: float, dataframe)
-    '''A função irá receber
-    
+    '''A função irá receber um tipo, um ingrediente, um preço, e irá gerar
+    uma nova linha em um dataframe previamente estabelecido.
+
+    :param tipo: tipo do ingrediente que será dado
+    :tipo type: str
+    :param ingrediente: ingrediente a ser adicionado na lista
+    :ingredientes type: str
+    :param preco: preço do ingrediente a ser adicionado
+    :preco type: float
+    :param dataframe: dataframe com a lista de ingredientes
+    :dataframe type: Pandas.dataframe
+    :return: Linha a mais do dataframe
     '''
